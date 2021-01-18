66fbab133c7502bd4e9a99c1c4d7713ee37c0aba
Michael Equi <equim20@student.jhs.net>
2021-01-18 12:36:27 -0800

    Added image warping

--- image_processing.py
+++ image_processing.py
@@ -1,17 +1,35 @@
 import cv2
-
+import numpy as np
+from pupil_apriltags import Detector
+import warp_image
 
 def main():
     print("Starting video")
+    measurements = []
     camera = cv2.VideoCapture(0)
+    at_detector = Detector(families='tagStandard41h12',
+                           nthreads=1,
+                           quad_decimate=1.0,
+                           quad_sigma=0.0,
+                           refine_edges=1,
+                           decode_sharpening=0.25,
+                           debug=0)
     while True:
         ret, frame = camera.read()
-        if not ret:
-            print("failed to grab frame")
-            break
-        cv2.imshow('video', frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
+        results = at_detector.detect(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+        if len(results) > 0:
+            measurements.append(results[0].corners)
+            if len(measurements) > 5:
+                m = np.array(measurements)
+                avg = np.average(m, axis=0)
+                dst = warp_image.warp_image(avg, frame)
+
+                if not ret:
+                    print("failed to grab frame")
+                    break
+                cv2.imshow('video', dst)
+                if cv2.waitKey(20) & 0xFF == ord('q'):
+                    break
     camera.release()
     cv2.destroyAllWindows()
 
