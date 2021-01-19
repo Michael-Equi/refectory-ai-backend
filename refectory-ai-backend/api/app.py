2396c93f0a78c12125c67c4fa2e40fb7d8f024e9
Michael Equi <equim20@student.jhs.net>
2021-01-19 09:30:53 -0800

    Added more to UI

--- refectory-ai-backend/api/app.py
+++ refectory-ai-backend/api/app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file, request
+import cv2
 import time
 
 app = Flask(__name__)
 
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/image')
+def get_image():
+    return send_file('/Users/michaelequi/Desktop/Screen Shot 2020-12-26 at 4.34.44 PM.png', mimetype='image/png')
+
+
+@app.route('/addAnnotation', methods=['POST'])
+def add_annotation():
+    if request.method == 'POST':
+        img = cv2.imread('/Users/michaelequi/Desktop/Screen Shot 2021-01-16 at 12.29.10 AM.png')
+        cv2.write('newimage.png', img)
+        print(request.json)
+        return send_file('newimage.png', mimetype='image/png')
--- refectory-ai-backend/src/App.js
+++ refectory-ai-backend/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import './styles/styles.css';
 
-function App() {
+const App = () => {
+
+  const [image, setImage] = useState(null);
+  const [clicks, setClicks] = useState([]);
+  const [isRound, setIsRound] = useState(false);
+
+  const nameRef = useRef();
+  const contentRef = useRef();
+
+  useEffect(() => {
+    if(image === null){
+      fetch('/image').then(res => {
+        if(!res.ok){
+          throw Error("Error fetching image");
+        }
+        setImage(res);
+      });
+    }
+  })
+
+  const imageClicked = (event) => {
+    setClicks([...clicks, [event.clientX, event.clientY]])
+  }
+
+  const sendForm = (e) => {
+    e.preventDefault();
+    if(clicks.length !== 2){
+      alert('Number of clicks should be 2')
+    } else {
+      const request = {
+        method:"POST",
+        cache: "no-cache",
+        headers:{
+          "content_type":"application/json",
+        },
+        body: JSON.stringify({
+          name: nameRef.current.value,
+          content: contentRef.current.value,
+          points: clicks
+        })
+      }
+      fetch('/addAnnotation', request).then(r => console.log(r));
+    }
+  }
+
+  /// Contents
+  /// image <-
+  /// name <- handle in form
+  /// round
+  /// section
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        image !== null ?
+          <img src={image.url} alt={''} onClick={imageClicked} style={{margin: 0, padding: 0}}/>
+          : null
+      }
+      <button>Undo</button>
+      <button>New Image</button>
+      <button onClick={() => setClicks([])}>Clear clicks</button>
+      {
+        isRound ?
+        <button onClick={() => {setIsRound(false)}}>Rectangle</button> :
+        <button onClick={() => {setIsRound(true)}}>Circle</button>
+      }
+      <br/>
+      <form onSubmit={sendForm}>
+        <label>
+          Points:
+          <ul>
+              {
+                clicks.map((click, idx) => (
+                  <li key={idx}>({click[0]}, {click[1]})</li>
+                ))
+              }
+          </ul>
+        </label>
+        <label>
+          Rounded: {isRound ? 'True' : 'False'}
+        </label>
+        <label>
+          Name:
+          <input ref={nameRef} type="text" name="name" />
+        </label>
+        <label>
+          Content:
+          <input type="text" ref={contentRef} name="content" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
